eda801d798841128f1aa0181d5f0a2d91c271634
Jeremy Gebhart <jeremy.gebhart@us.sogeti.com>
2018-05-16 15:17:01 -0400

    Adding intial .NET Core project and the client page.

---
+++ api/dot_net/DotNetCoreApi/DotNetCoreApi/Controllers/PlanetsController.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreApi.Controllers
+{
+    [Route("api/planets")]
+    public class PlanetsController : Controller
+    {
+        ILogger<PlanetsController> _logger;
+
+        public PlanetsController(ILogger<PlanetsController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get the star charts.
+        /// </summary>
+        /// <returns>List of all planets in the star charts.</returns>
+        [HttpGet()]
+        public IActionResult GetPlanets()
+        {
+            _logger.LogInformation("Returning all planets...");
+            return Ok(PlanetsDataStore.Current.Planets);
+        }
+
+        /// <summary>
+        /// Get a single planet from the star charts.
+        /// </summary>
+        /// <param name="planetId">Identifier for the requested planet.</param>
+        /// <returns>The requested planetary record.</returns>
+        [HttpGet("{planetId}", Name = "GetPlanet")]
+        public IActionResult GetPlanet(int planetId)
+        {
+            // Find the planet
+            var planet = PlanetsDataStore.Current.Planets.FirstOrDefault(p => p.Id == planetId);
+            if (planet == null)
+            {
+                _logger.LogError($"Planet with id {planetId} was not found.");
+                return NotFound($"Planet with id {planetId} was not found.");
+            }
+            _logger.LogInformation($"Planet {planet.Name} with id {planet.Id} has been located.");
+
+            return Ok(planet);
+        }
+
+        /// <summary>
+        /// Add a new planet to the star charts.
+        /// </summary>
+        /// <param name="planetToAdd">Planet to add.</param>
+        /// <returns>Route to the new planet, and the planet that was added.</returns>
+        [HttpPost()]
+        public IActionResult AddPlanet([FromBody] PlanetInfo planetToAdd)
+        {
+            // Error checking
+            if (planetToAdd == null)
+            {
+                _logger.LogError("No planet information was received.");
+                return BadRequest("No planet information was received.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Invalid PlanetInfo model.");
+                return BadRequest(ModelState);
+            }
+
+            // Create new planet
+            // In a system backed by a database, the id increment would be handled by the db
+            var lastId = PlanetsDataStore.Current.Planets.Select(p => p.Id).Max();
+            var newPlanet = new PlanetInfo()
+            {
+                Id = ++lastId, 
+                Name = planetToAdd.Name,
+                Description = planetToAdd.Description
+            };
+            PlanetsDataStore.Current.Planets.Add(newPlanet);
+            _logger.LogInformation($"Planet {planetToAdd.Name} sucessfully added.");
+
+            return CreatedAtRoute("GetPlanet", new
+            { planetId = newPlanet.Id }, newPlanet);
+        }
+
+        /// <summary>
+        /// Update the star charts with the planet specified.
+        /// </summary>
+        /// <param name="planetId">Identifier for the planet to be updated.</param>
+        /// <param name="planetToUpdate">Updated planetary information.</param>
+        /// <returns>Confirmation status that the star charts have been updated.</returns>
+        [HttpPut("{planetId}")]
+        public IActionResult UpdatePlanet(int planetId,
+            [FromBody] PlanetInfo planetToUpdate)
+        {
+            // Error checking
+            if (planetToUpdate == null)
+            {
+                _logger.LogError("No planet information was received.");
+                return BadRequest("No planet information was received.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogError("Invalid PlanetInfo model.");
+                return BadRequest(ModelState);
+            }
+
+            // Update the specified planet
+            var planet = PlanetsDataStore.Current.Planets.FirstOrDefault(p => p.Id == planetId);
+            if (planet == null)
+            {
+                _logger.LogError($"Unable to locate planet with id {planetId}.");
+                return NotFound($"Unable to locate planet with id {planetId}.");
+            }
+            planet.Name = planetToUpdate.Name;
+            planet.Description = planetToUpdate.Description;
+            _logger.LogInformation($"The star chart entry for {planet.Name} has been update.");
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Delete the specified planet from the star charts.
+        /// </summary>
+        /// <param name="planetId">Identifier for the planet to be removed.</param>
+        /// <returns>Confirmation status that the star charts have been updated.</returns>
+        [HttpDelete("{planetId}")]
+        public IActionResult DeletePlanet(int planetId)
+        {
+            // Find the planet
+            var planet = PlanetsDataStore.Current.Planets.FirstOrDefault(p => p.Id == planetId);
+            if (planet == null)
+            {
+                _logger.LogError($"Planet with id {planetId} was not found.");
+                return NotFound($"Planet with id {planetId} was not found.");
+            }
+
+            PlanetsDataStore.Current.Planets.Remove(planet);
+            _logger.LogInformation($"{planet.Name} has been deleted from the star charts.");
+
+            return NoContent();
+        }
+    }
+}
---
+++ api/dot_net/DotNetCoreApi/DotNetCoreApi/PlanetsDataStore.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreApi
+{
+    public class PlanetsDataStore
+    {
+        public static PlanetsDataStore Current { get; } = new PlanetsDataStore();
+        public ICollection<PlanetInfo> Planets { get; set; }
+
+        public PlanetsDataStore()
+        {
+            Planets = new List<PlanetInfo>()
+            {
+                new PlanetInfo()
+                {
+                    Id = 1,
+                    Name = "Coruscant",
+                    Description = "Galactic Capital"
+                },
+                new PlanetInfo()
+                {
+                    Id = 2,
+                    Name = "Corellia",
+                    Description = "Imperial Capital Ship Construction"
+                },
+                new PlanetInfo()
+                {
+                    Id = 3,
+                    Name = "Mandalore",
+                    Description = "Homeworld of the Mandalorian Culture"
+                },
+                new PlanetInfo()
+                {
+                    Id = 4,
+                    Name = "Cantonica",
+                    Description = "Home of Canto Bite"
+                },
+                new PlanetInfo()
+                {
+                    Id = 5,
+                    Name = "Yavin 4",
+                    Description = "Rebel Base"
+                }
+            };
+        }
+    }
+}
