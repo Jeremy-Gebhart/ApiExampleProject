3083444b92083dcd9ff695efc4a2d16ef0e9b117
Jeremy Gebhart <jeremy.gebhart@us.sogeti.com>
2018-05-22 15:53:35 -0400

    Setting up to add a TypeScript NodeJs server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,30 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('moveJquery', function() {
+// File moving tasks
+gulp.task('moveJquery', () => {
     return gulp.src('node_modules/jquery/dist/*.js')
         .pipe(gulp.dest('js/lib'));
 });
-
-gulp.task('moveRequire', function() {
+gulp.task('moveRequire', () => {
     return gulp.src('node_modules/requirejs/require.js')
         .pipe(gulp.dest('js/lib'))
 });
-
-gulp.task('moveTextJs', function() {
+gulp.task('moveTextJs', () => {
     return gulp.src('node_modules/requirejs-text/text.js')
         .pipe(gulp.dest('js/lib'));
 });
+
+// Typescript tasks
+gulp.task('scripts', () => {
+    const tsResult = tsProject.src()
+    .pipe(tsProject());
+    return tsResult.js.pipe(gulp.dest('dist'));
+});
+gulp.task('watch', ['scripts'], () => {
+    gulp.watch('api/nodejs/**/*.ts', ['scripts']);
+});
+
 
 gulp.task('default', ['moveJquery', 'moveRequire', 'moveTextJs']);
